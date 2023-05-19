838cbd3b194784c12450cafeb6725028719254cf
Kirkakiray <132487836+Kirkakiray@users.noreply.github.com>
2023-05-19 17:25:39 +0300

    Add files via upload
    v 5

--- Escape from house.py
+++ Escape from house.py
@@ -1,5 +1,9 @@
 from pygame import *
-from random import randint
+from random import randint 
+from time import time as timer
+
+
+
 window = display.set_mode((700, 500))
 display.set_caption('Escape from house')
 background = transform.scale(image.load('wall.jpg'), (700, 500))
@@ -9,6 +13,20 @@ FPS = 50
 win_width = 700
 win_height = 500
 score = 0
+goal = 10
+
+
+'''mixer.init()
+mixer.music.load('space.mp3')
+mixer.music.play()
+sound = mixer.Sound('space.ogg')'''
+
+
+font.init()
+font2 = font.Font(None, 36)
+font1 = font.Font(None, 36)
+win = font1.render('ТЫ СБЕЖАЛ! ТЕПЕРЬ БЕГИ К МАМЕ!', True, (225, 255, 255))
+lose = font1.render('О ЧЁРТ! ТЫ НЕ СБЕЖАЛ, ТЕБЯ СЪЕЛА БАБАЙКА!', True, (180, 0, 0))
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -38,9 +56,11 @@ class Player(GameSprite):
 
 
 
-player = Player(('gg.png'), 50, 400, 80, 80, 4)
-door = GameSprite(('door.png'), randint(20, 680), randint(20, 480), 50, 50, 0)
-game = True 
+player = Player(('gg.png'), 50, 400, 80, 80, 3)
+door = GameSprite(('door.png'), randint(20, 680), randint(20, 480), 100, 100, 0)
+
+time = 20
+game = True
 finish = False
 while game:
     for e in event.get():
@@ -54,8 +74,25 @@ while game:
         door.update()
         door.reset()
 
-    
 
+
+        if sprite.collide_rect(player, door):
+            score = score + 1
+            door = GameSprite(('door.png'), randint(20, 660), randint(20, 460), 100, 100, 0)
+            '''sound.play()'''
+        
+        ''' window.blit(lose,(10, 50))'''
+            
+
+        if score >= goal:
+            finish = True
+            window.blit(win, (200, 200))
+        text = font2.render('Score:' + str(score), 1, (225, 225, 255))
+        window.blit(text, (10, 20))
+       
+
+
+    
 
 
     display.update()
